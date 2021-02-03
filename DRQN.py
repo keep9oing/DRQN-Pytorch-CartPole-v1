23f47c91c05a6630b99c7415386d10b593f4bd1c
keep9oing <jungminash@gmail.com>
2021-02-03 12:46:05 +0900

    Add random update, Generalizability. Modified README

--- DRQN.py
+++ DRQN.py
@@ -29,14 +29,14 @@ class Q_net(nn.Module):
         self.action_space = action_space
 
         self.Linear1 = nn.Linear(self.state_space, self.hidden_space)
-        self.lstm    = nn.LSTM(self.hidden_space,self.hidden_space)
+        self.lstm    = nn.LSTM(self.hidden_space,self.hidden_space, batch_first=True)
         self.Linear2 = nn.Linear(self.hidden_space, self.action_space)
 
     def forward(self, x, h, c):
         x = F.relu(self.Linear1(x))
         x, (new_h, new_c) = self.lstm(x,(h,c))
         x = self.Linear2(x)
-        return x.squeeze(), new_h, new_c
+        return x, new_h, new_c
 
     def sample_action(self, obs, h,c, epsilon):
         output = self.forward(obs, h,c)
@@ -46,19 +46,27 @@ class Q_net(nn.Module):
         else:
             return output[0].argmax().item(), output[1] , output[2]
     
-    def init_hidden_state(self, batch_size):
-        return torch.zeros([1,batch_size, self.hidden_space]), torch.zeros([1, batch_size, self.hidden_space])
+    def init_hidden_state(self, batch_size, training=None):
+
+        assert training is not None, "training step parameter should be dtermined"
+
+        if training is True:
+            return torch.zeros([1, batch_size, self.hidden_space]), torch.zeros([1, batch_size, self.hidden_space])
+        else:
+            return torch.zeros([1, 1, self.hidden_space]), torch.zeros([1, 1, self.hidden_space])
 
 class EpisodeMemory():
     """Episode memory for recurrent agent"""
 
     def __init__(self, random_update=False, 
                        max_epi_num=100, max_epi_len=500,
-                       batch_size=1):
+                       batch_size=1,
+                       lookup_step=None):
         self.random_update = random_update # if False, sequential update
         self.max_epi_num = max_epi_num
         self.max_epi_len = max_epi_len
         self.batch_size = batch_size
+        self.lookup_step = lookup_step
 
         if (random_update is False) and (self.batch_size > 1):
             sys.exit('It is recommend to use 1 batch for sequential update, if you want, erase this code block and modify code')
@@ -69,15 +77,34 @@ class EpisodeMemory():
         self.memory.append(episode)
 
     def sample(self):
-        sampled_episodes = []
+        sampled_buffer = []
 
+        ##################### RANDOM UPDATE ############################
         if self.random_update: # Random upodate
-            pass
+            sampled_episodes = random.sample(self.memory, self.batch_size)
+            
+            check_flag = True # check if every sample data to train is larger than batch size
+            min_step = self.max_epi_len
+
+            for episode in sampled_episodes:
+                min_step = min(min_step, len(episode)) # get minimum step from sampled episodes
+
+            for episode in sampled_episodes:
+                if min_step > self.lookup_step: # sample buffer with lookup_step size
+                    idx = np.random.randint(0, len(episode)-self.lookup_step+1)
+                    sample = episode.sample(random_update=self.random_update, lookup_step=self.lookup_step, idx=idx)
+                    sampled_buffer.append(sample)
+                else:
+                    idx = np.random.randint(0, len(episode)-min_step+1) # sample buffer with minstep size
+                    sample = episode.sample(random_update=self.random_update, lookup_step=min_step, idx=idx)
+                    sampled_buffer.append(sample)
+
+        ##################### SEQUENTIAL UPDATE ############################           
         else: # Sequential update
             idx = np.random.randint(0, len(self.memory))
-            sampled_episodes.append(self.memory[idx].sample())
+            sampled_buffer.append(self.memory[idx].sample(random_update=self.random_update))
 
-        return sampled_episodes
+        return sampled_buffer, len(sampled_buffer[0]['obs']) # buffers, sequence_length
 
     def __len__(self):
         return len(self.memory)
@@ -100,12 +127,19 @@ class EpisodeBuffer:
         self.next_obs.append(transition[3])
         self.done.append(transition[4])
 
-    def sample(self) -> Dict[str, np.ndarray]:
+    def sample(self, random_update=False, lookup_step=None, idx=None) -> Dict[str, np.ndarray]:
         obs = np.array(self.obs)
         action = np.array(self.action)
         reward = np.array(self.reward)
         next_obs = np.array(self.next_obs)
         done = np.array(self.done)
+
+        if random_update is True:
+            obs = obs[idx:idx+lookup_step]
+            action = action[idx:idx+lookup_step]
+            reward = reward[idx:idx+lookup_step]
+            next_obs = next_obs[idx:idx+lookup_step]
+            done = done[idx:idx+lookup_step]
 
         return dict(obs=obs,
                     acts=action,
@@ -127,29 +161,44 @@ def train(q_net=None, target_q_net=None, episode_memory=None,
     assert device is not None, "None Device input: device should be selected."
 
     # Get batch from replay buffer
-    samples = episode_memory.sample()
+    samples, seq_len = episode_memory.sample()
+
+    observations = []
+    actions = []
+    rewards = []
+    next_observations = []
+    dones = []
 
     for i in range(batch_size):
-        observations = torch.FloatTensor(samples[i]["obs"]).to(device)
-        actions = torch.LongTensor(samples[i]["acts"].reshape(-1,1)).to(device)
-        rewards = torch.FloatTensor(samples[i]["rews"].reshape(-1,1)).to(device)
-        next_observations = torch.FloatTensor(samples[i]["next_obs"]).to(device)
-        dones = torch.FloatTensor(samples[i]["done"].reshape(-1,1)).to(device)
+        observations.append(samples[i]["obs"])
+        actions.append(samples[i]["acts"])
+        rewards.append(samples[i]["rews"])
+        next_observations.append(samples[i]["next_obs"])
+        dones.append(samples[i]["done"])
 
-    h_target, c_target = target_q_net.init_hidden_state(batch_size)
-    # Define loss
-    if episode_memory.random_update:
-        pass
-    else:
-        q_target, _, _ = target_q_net(next_observations.unsqueeze(1), h_target.to(device), c_target.to(device))
+    observations = np.array(observations)
+    actions = np.array(actions)
+    rewards = np.array(rewards)
+    next_observations = np.array(next_observations)
+    dones = np.array(dones)
 
-    q_target_max = q_target.max(1)[0].unsqueeze(1).detach()
+    observations = torch.FloatTensor(observations.reshape(batch_size,seq_len,-1)).to(device)
+    actions = torch.LongTensor(actions.reshape(batch_size,seq_len,-1)).to(device)
+    rewards = torch.FloatTensor(rewards.reshape(batch_size,seq_len,-1)).to(device)
+    next_observations = torch.FloatTensor(next_observations.reshape(batch_size,seq_len,-1)).to(device)
+    dones = torch.FloatTensor(dones.reshape(batch_size,seq_len,-1)).to(device)
+
+    h_target, c_target = target_q_net.init_hidden_state(batch_size=batch_size, training=True)
+
+    q_target, _, _ = target_q_net(next_observations, h_target.to(device), c_target.to(device))
+
+    q_target_max = q_target.max(2)[0].view(batch_size,seq_len,-1).detach()
     targets = rewards + gamma*q_target_max*dones
 
 
-    h, c = q_net.init_hidden_state(batch_size)
-    q_out, _, _ = q_net(observations.unsqueeze(1), h.to(device), c.to(device))
-    q_a = q_out.gather(1, actions)
+    h, c = q_net.init_hidden_state(batch_size=batch_size, training=True)
+    q_out, _, _ = q_net(observations, h.to(device), c.to(device))
+    q_a = q_out.gather(2, actions)
 
     # Multiply Importance Sampling weights to loss        
     loss = F.smooth_l1_loss(q_a, targets)
@@ -170,8 +219,8 @@ def save_model(model, path='default.pth'):
 
 if __name__ == "__main__":
 
-    # Determine seeds
-    model_name = "DRQN_POMDP_SEQ"
+    # Env parameters
+    model_name = "DRQN_POMDP_Random"
     env_name = "CartPole-v1"
     seed = 1
     exp_num = 'SEED'+'_'+str(seed)
@@ -182,6 +231,7 @@ if __name__ == "__main__":
     if torch.cuda.is_available():
         device = torch.device("cuda")
 
+    # Set the seed
     np.random.seed(seed)
     random.seed(seed)
     seed_torch(seed)
@@ -191,10 +241,10 @@ if __name__ == "__main__":
     writer = SummaryWriter('runs/'+env_name+"_"+model_name+"_"+exp_num)
 
     # Set parameters
-    batch_size = 1
+    batch_size = 8
     learning_rate = 1e-3
     buffer_len = int(100000)
-    min_epi_num = 10
+    min_epi_num = 20 # Start moment to train the Q network
     episodes = 650
     print_per_iter = 20
     target_update_period = 4
@@ -205,7 +255,12 @@ if __name__ == "__main__":
     max_step = 2000
 
     # DRQN param
-    update_step = 5 # If you want to do random update instead of sequential update
+    random_update = True# If you want to do random update instead of sequential update
+    lookup_step = 20 # If you want to do random update instead of sequential update
+    max_epi_len = 100 
+    max_epi_step = max_step
+
+    
 
     # Create Q functions
     Q = Q_net(state_space=env.observation_space.shape[0]-2, 
@@ -222,7 +277,10 @@ if __name__ == "__main__":
 
     epsilon = eps_start
     
-    episode_memory = EpisodeMemory(random_update=False, max_epi_num=100, max_epi_len=600, batch_size=batch_size)
+    episode_memory = EpisodeMemory(random_update=random_update, 
+                                   max_epi_num=100, max_epi_len=600, 
+                                   batch_size=batch_size, 
+                                   lookup_step=lookup_step)
 
     # Train
     for i in range(episodes):
@@ -231,7 +289,7 @@ if __name__ == "__main__":
         done = False
         
         episode_record = EpisodeBuffer()
-        h, c = Q.init_hidden_state(batch_size)
+        h, c = Q.init_hidden_state(batch_size=batch_size, training=False)
 
         for t in range(max_step):
 
--- README.md
+++ README.md
@@ -2,16 +2,39 @@
 
 Reference: https://arxiv.org/pdf/1507.06527.pdf
 
+1. Pytorch(1.5.0)
+2. Openai Gym(0.17.1)
+3. Tensorboard (2.1.0)
+
 ## Training envrionment: OpenAI gym (CartPolev1)
 
+&nbsp;
+&nbsp;
+
 <img src="./assets/cartpolev1.png" align="center"></img>
+
 * * *
 ## POMDP
+
 - CartPole-v1 environment is consists of Cart position/velocity, Pole angle/velocity.
+&nbsp;
+
 <img src="./assets/cartpolestate.png" align="center"></img>
-- I set the partially observed state is position of cart and pole's position. The agent doesn't have idea of the velocity.
+- I set the partially observed state is position of cart and pole's angle. __The agent doesn't have idea of the velocity.__
+* * *
+## Stable Recurrent Updates
+### 1. Bootstrapped Sequential Updates
+- episodes are selected randomly from the replay memory and updates begin at the beginning of the episode and proceed forward through time to the conclusion of the episode. The targets at each timestep are generated from the target Q-network. The RNN's hidden state is carried forward throughout episode.
+### 2. Sequential update
+- Episodes are selected randomly from the replay memory and updates begin at random points in the episode and proceed for only unroll iterations timesteps(lookup_step). The targets at each timestep are generated from the target Q-network. __The RNN's initial state is zeroed at the start of the update.__
+
+<img src="./assets/DRQN_param.png" align="center"></img>
+
+- The above parameters are used to set the DRQN setting. __random update__ choose what update method to use.
+- __lookup_step__ is how long step to observe. I found that longer lookup_step is better.
 * * *
 ## DQN with Fully Oberserved vs DQN with POMDP vs DRQN with POMDP
+&nbsp;
 <img src="./assets/rewardlog.png" align="center" height="400px"></img>
 - (orange)DQN with fully observed MDP situation can reach the highest reward.
 - (blue)DQN with POMDP never can be reached to the high reward situation.
@@ -19,4 +42,4 @@ Reference: https://arxiv.org/pdf/1507.06527.pdf
 
 
 ### TODO
-- [ ] Random update of DRQN
+- [x] Random update of DRQN
